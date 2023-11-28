6aa4e6e3b6e248d3cc2d6aa050c679af751406e2
usamasadiqus <usamasadiq029@gmail.com>
2023-11-28 15:18:54 +0500

    update all routes with model and database functionality

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,45 +1,137 @@
 const { Router } = require("express");
+const asyncHandler = require("express-async-handler");
+const Contact = require("./../models/Contact");
 
 const router = Router();
 
 // Get all contacts
-router.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "Get All Contacts",
-  });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const contacts = await Contact.find();
+
+    res.status(200).json({
+      status: "success",
+      results: contacts.length,
+      data: { contacts },
+    });
+  })
+);
 
 // Get contact
-router.get("/:id", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: `Get contact by id: ${req.params.id}`,
-  });
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      res.status(400).json({ status: "error", message: "Contact not found!" });
+      return;
+    }
+
+    res.status(200).json({ status: "success", data: { contact } });
+  })
+);
 
 // Create new contact
-router.post("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: `Create contact successfully.`,
-  });
-});
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, email, phone, subject, category, message } = req.body;
+
+    // Validate fields
+    if (!name || !email || !phone || !subject || !category || !message) {
+      res
+        .status(400)
+        .json({ status: "error", message: "All fields must be filled!" });
+      return;
+    }
+
+    if (
+      ![
+        "groups_and_charters",
+        "become_reseller",
+        "become_affiliate",
+        "customer_service",
+        "feedback",
+        "press_inquiry",
+        "others",
+      ].includes(category)
+    ) {
+      res.status(400).json({ status: "error", message: "Invalid category!" });
+      return;
+    }
+
+    const newContact = await Contact.create(req.body);
+
+    newContact &&
+      res.status(201).json({
+        status: "success",
+        message: "Thanks for your message.",
+      });
+  })
+);
 
 // Update contact
-router.patch("/:id", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: `Update contact by id: ${req.params.id}`,
-  });
-});
+router.patch(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const { name, email, phone, subject, category, message } = req.body;
+
+    // Validate fields
+    if (!name || !email || !phone || !subject || !category || !message) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "All fields must be filled!" });
+    }
+
+    if (
+      ![
+        "groups_and_charters",
+        "become_reseller",
+        "become_affiliate",
+        "customer_service",
+        "feedback",
+        "press_inquiry",
+        "others",
+      ].includes(category)
+    ) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "Invalid category!" });
+    }
+
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!contact) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "Contact not found!" });
+    }
+
+    res.status(200).json({ status: "success", data: { contact } });
+  })
+);
 
 // Delete contact
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: `Delete contact by id: ${req.params.id}`,
-  });
-});
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const contact = await Contact.findByIdAndDelete(req.params.id);
+
+    if (!contact) {
+      res.status(400).json({ status: "error", message: "Contact not found!" });
+      return;
+    }
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  })
+);
 
 module.exports = router;
