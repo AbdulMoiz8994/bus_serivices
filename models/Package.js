8cd9f7eaeec0a25f28df5afcface85a4f16c79de
usamasadiqus <usamasadiq029@gmail.com>
2023-12-07 01:20:54 +0500

    update package model

--- models/Package.js
+++ models/Package.js
@@ -5,7 +5,16 @@ const packageSchema = new mongoose.Schema(
     type: {
       type: String,
       required: [true, "A package must have a package type!"],
-      enum: ["Hop-On Hop-Off Sightseeing Passes", "Liberty Cruises"], // Choose from available types
+      enum: [
+        "Attraction Pass",
+        "City Lights Cruise",
+        "Hop-On Hop-Off Sightseeing Passes",
+        "Liberty Cruises",
+        "Night Tours",
+        "North Pole Express",
+        "NYC Sightseeing",
+        "Sunset Cruise",
+      ], // Choose from available types
       trim: true,
     },
     tag: {
@@ -24,13 +33,12 @@ const packageSchema = new mongoose.Schema(
       trim: true,
     },
     price: {
-      type: String,
+      type: Number,
       required: [true, "A package must have a price!"],
-      trim: true,
     },
     perDayPrice: {
-      type: String,
-      trim: true,
+      type: Number,
+      default: 0,
     },
     dealPercentage: {
       type: String,
@@ -38,118 +46,40 @@ const packageSchema = new mongoose.Schema(
       trim: true,
     },
     dealPrice: {
-      type: String,
+      type: Number,
       required: [true, "A package must have a deal price!"],
-      trim: true,
     },
     ticketSold: {
       type: Number,
       default: 0,
     },
-    isHopOnHopOffDowntownVisible: {
-      type: Boolean,
-      default: true,
-    },
-    hopOnHopOffDowntown: {
-      type: Boolean,
-      default: true,
-    },
-    isHopOnHopOffUptownVisible: {
-      type: Boolean,
-      default: true,
-    },
-    hopOnHopOffUptown: {
-      type: Boolean,
-      default: true,
-    },
-    isStatueofLibertyCruiseVisible: {
-      type: Boolean,
-      default: true,
-    },
-    statueofLibertyCruise: {
-      type: Boolean,
-      default: true,
-    },
-    isLibertyCruisePremiumVisible: {
-      type: Boolean,
-      default: false,
-    },
-    libertyCruisePremium: {
-      type: Boolean,
-      default: false,
-    },
-    isLibertyCruiseStandardVisible: {
-      type: Boolean,
-      default: false,
-    },
-    libertyCruiseStandard: {
-      type: Boolean,
-      default: false,
-    },
-    isHolidayLightsTourwithEntertainingHostVisible: {
-      type: Boolean,
-      default: true,
-    },
-    holidayLightsTourwithEntertainingHost: {
-      type: Boolean,
-      default: true,
-    },
-    isCentralParkBikeRentalVisible: {
-      type: Boolean,
-      default: true,
-    },
-    centralParkBikeRental: {
-      type: Boolean,
-      default: true,
-    },
-    isBrooklynTourVisible: {
-      type: Boolean,
-      default: true,
-    },
-    brooklynTour: {
-      type: Boolean,
-      default: true,
-    },
-    isMuseumoftheCityofNewYorkVisible: {
-      type: Boolean,
-      default: true,
-    },
-    museumoftheCityofNewYork: {
-      type: Boolean,
-      default: true,
-    },
-    isSkyscraperMuseumVisible: {
-      type: Boolean,
-      default: true,
-    },
-    skyscraperMuseum: {
-      type: Boolean,
-      default: true,
-    },
-    isHarryPotterStoreFreeTankardofButterbeerVisible: {
-      type: Boolean,
-      default: true,
-    },
-    harryPotterStoreFreeTankardofButterbeer: {
-      type: Boolean,
-      default: true,
-    },
-    isMobileboardingVisible: {
-      type: Boolean,
-      default: true,
-    },
-    mobileboarding: {
-      type: Boolean,
-      default: true,
-    },
-    isMoviesCelebritiesaudiochannelVisible: {
-      type: Boolean,
-      default: true,
-    },
-    moviesCelebritiesaudiochannel: {
-      type: Boolean,
-      default: true,
-    },
+    details: [
+      {
+        fieldName: {
+          type: String,
+          required: [true, "A package must have a package details!"],
+          trim: true,
+        },
+        fieldValue: {
+          type: Boolean,
+          required: [true, "A package must have a package details!"],
+        },
+      },
+    ],
+    extras: [
+      {
+        fieldName: {
+          type: String,
+          required: [true, "A package must have a package extras!"],
+          trim: true,
+        },
+        fieldValue: {
+          type: Boolean,
+          required: [true, "A package must have a package extras!"],
+          default: true,
+        },
+      },
+    ],
     isVisible: {
       type: Boolean,
       default: true,
