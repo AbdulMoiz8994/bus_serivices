cf5154f1e23ee1bb36a29bb5908baac46760b9dd
usamasadiqus <usamasadiq029@gmail.com>
2023-12-10 02:42:15 +0500

    update create order functionality with add nodemailer

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const asyncHandler = require("express-async-handler");
 const Order = require("./../models/Order");
+const nodemailer = require("nodemailer");
 
 const router = Router();
 
@@ -37,10 +38,30 @@ router.get(
 router.post(
   "/",
   asyncHandler(async (req, res) => {
-    const { package, date, firstName, lastName, email } = req.body;
+    const {
+      title,
+      date,
+      time,
+      adults,
+      adultsPrice,
+      kids,
+      kidsPrice,
+      totalPrice,
+      firstName,
+      lastName,
+      email,
+    } = req.body;
 
     // Validate fields
-    if (!package || !date || !firstName || !lastName || !email) {
+    if (
+      !title ||
+      !date ||
+      !time ||
+      !totalPrice ||
+      !firstName ||
+      !lastName ||
+      !email
+    ) {
       res
         .status(400)
         .json({ status: "error", message: "All fields must be filled!" });
@@ -49,11 +70,140 @@ router.post(
 
     const newOrder = await Order.create(req.body);
 
-    newOrder &&
+    if (newOrder) {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER_EMAIL,
+          pass: process.env.USER_PASS,
+        },
+      });
+
+      // Send mail via nodemailer
+      const mailOptions = {
+        from: process.env.USER_EMAIL, // sender email
+        to: email, // receiver email
+        subject: "Order Confirmation",
+        html: `<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Bus Booking Confirmation</title>
+            <style>
+                body {
+                    font-family: 'Arial', sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color: #f1f1f1;
+                }
+        
+                .container {
+                    max-width: 600px;
+                    margin: 20px auto;
+                    background-color: #fff;
+                    padding: 20px;
+                    border-radius: 10px;
+                }
+        
+                h2 {
+                    color: #007BFF;
+                }
+        
+                p {
+                    margin: 0 0 20px;
+                }
+        
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin-top: 20px;
+                }
+        
+                table,
+                th,
+                td {
+                    border: 1px solid #ddd;
+                }
+        
+                th,
+                td {
+                    padding: 15px;
+                    text-align: left;
+                }
+        
+                th {
+                    background-color: #007BFF;
+                    color: #fff;
+                }
+        
+                .footer {
+                    margin-top: 20px;
+                    text-align: center;
+                }
+            </style>
+        </head>
+        
+        <body>
+            <div class="container">
+                <h2>Bus Booking Confirmation</h2>
+                <p>Dear ${firstName} ${lastName},</p>
+        
+                <p>Your bus booking details are confirmed. Please find the booking details below:</p>
+        
+                <table>
+                    <tr>
+                        <th>Package</th>
+                        <td>${title}</td>
+                    </tr>
+                    <tr>
+                        <th>Date</th>
+                        <td>${date}</td>
+                    </tr>
+                    <tr>
+                        <th>Time</th>
+                        <td>${time}</td>
+                    </tr>
+                    <tr>
+                        <th>Adults</th>
+                        <td>${adults} - $${adultsPrice}</td>
+                    </tr>
+                    <tr>
+                        <th>Kids</th>
+                        <td>${kids} - $${kidsPrice}</td>
+                    </tr>
+                    <tr>
+                        <th>Total Price</th>
+                        <td>$${totalPrice}</td>
+                    </tr>
+                </table>
+        
+                <p style="margin-top: 20px;">Thank you for choosing our bus service. We look forward to serving you!</p>
+        
+                <div class="footer">
+                    <p>If you have any questions, please contact us at ....</p>
+                </div>
+            </div>
+        </body>
+        
+        </html>`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error("error >>>>>>>>>>>>>", error);
+        } else {
+          console.log("info >>>>>>", info);
+          console.info("Email sent: " + info.response);
+        }
+      });
+
       res.status(201).json({
         status: "success",
         message: "Your order has been placed successfully.",
       });
+    }
   })
 );
 
