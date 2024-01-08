088710a45e60ff4c30401eb294f997f73047b16d
AbdulMoiz8994 <moiza8994@gmail.com>
2024-01-08 15:57:13 +0500

    add th chnages

--- routes/auth.js
+++ routes/auth.js
@@ -13,25 +13,28 @@ router.post(
     console.log("password", password);
     try {
       const salt = 10;
-      const hashPassword =await bycrypt.hash(password, bycrypt.genSaltSync(10));
-       console.log(hashPassword);
-       const existingPassword = await Password.findOne();
-    //    check if the existing password save then update with the new one
-       if (existingPassword) {
+      const hashPassword = await bycrypt.hash(
+        password,
+        bycrypt.genSaltSync(10)
+      );
+      console.log(hashPassword);
+      const existingPassword = await Password.findOne();
+      //    check if the existing password save then update with the new one
+      if (existingPassword) {
         console.log("ex", existingPassword);
-         existingPassword.password = hashPassword;
-         await existingPassword.save();
-       } else {
-         await Password.create({
-            password: hashPassword
-         });
-       }
-   
-       return res
-         .status(201)
-         .json({ message: 'The Password has been set successfully' });
+        existingPassword.password = hashPassword;
+        await existingPassword.save();
+      } else {
+        await Password.create({
+          password: hashPassword,
+        });
+      }
+
+      return res
+        .status(201)
+        .json({ msg: "The Password has been set successfully" });
     } catch (err) {
-    return    res
+      return res
         .status(500)
         .json({ msg: "The Password is not set successfully" });
     }
@@ -39,3 +42,27 @@ router.post(
 );
 
 module.exports = router;
+
+
+
+router.post("/get-password", asyncHandler(async(req, res) =>{
+   const {password}= req.body;
+
+    try{
+        const getPassword=await Password.findOne();
+
+      if(!getPassword){
+        res.status(401).json({msg: "The Password is not set!"})   
+      }
+      let checkPassword=await bycrypt.compare(password, getPassword.password);
+      if(checkPassword){
+        res.status(200).json({msg: "Successfully Login"})
+      }else{
+        res.status(401).json({msg: "Password does not match"})
+      }
+
+    }catch(err){
+        return res.send(500).json({msg: "Error Authentication"})
+    }
+
+}))
