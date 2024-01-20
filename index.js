d21cbfddc748a7423573a5fd73d73de8a6b004df
AbdulMoiz8994 <moiza8994@gmail.com>
2024-01-21 01:59:38 +0500

    add the admi control cors

--- index.js
+++ index.js
@@ -39,6 +39,28 @@ app.use(cors(corsConfig))
 app.options('*', cors(corsConfig));
 
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 
 // app.all("/*", function (req, res, next) {
 //   const allowedOrigins = ['http://localhost:3000', 'https://travelapp-m1iq.vercel.app'];
