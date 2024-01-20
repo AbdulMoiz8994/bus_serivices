53b9476cfc827d9659c5e760072158f7c57e3371
AbdulMoiz8994 <moiza8994@gmail.com>
2024-01-21 02:17:16 +0500

    add the admi control cors

--- index.js
+++ index.js
@@ -24,42 +24,38 @@ const server = http.createServer(app);
 
 // console.log("process.env >>>>>", process.env);
 
-app.use(express.json({ limit: "10mb" }));
-
-// Middleware to parse URL-encoded data from form submissions
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Setup Cors
-const corsConfig = {
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options('*', cors(corsConfig));
+// const corsConfig = {
+//   origin: '*',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(corsConfig))
+// app.options('*', cors(corsConfig));
 
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 
 // app.all("/*", function (req, res, next) {
@@ -80,6 +76,18 @@ app.use(function (req, res, next) {
 // const app = require("./app");
 const { connectDB } = require("./config/db");
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+
+app.use(express.json({ limit: "10mb" }));
+
+// Middleware to parse URL-encoded data from form submissions
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const runningEnvironment = process.env.NODE_ENV;
 
