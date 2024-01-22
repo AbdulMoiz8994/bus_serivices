695cdcd65dc9b0b2f0361a1dfac571565b0af2c2
AbdulMoiz8994 <moiza8994@gmail.com>
2024-01-22 20:12:52 +0500

    add the admi control cors

--- index.js
+++ index.js
@@ -41,21 +41,21 @@ app.options('*', cors(corsConfig));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
- 
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://nyiconictours.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://nyiconictours.com");
+  // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+  // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "*"
   );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
 
   // Pass to next layer of middleware
   next();
